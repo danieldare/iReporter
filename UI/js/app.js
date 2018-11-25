dde9e4ab13b0f5ebee10acab6dd0e092d5a71ada
danieldare <dodanieloluwadare@gmail.com>
2018-11-25 23:54:01 +0100

    Dashboard page Updated

--- UI/js/app.js
+++
@@ -1,7 +0,0 @@
-const redFlagItems= document.getElementById("sub-option");
-const redFlagAction = document.getElementById('redFlagAction');
-redFlagAction.addEventListener('click',toggleRedFlagItems);
-function toggleRedFlagItems(e){
-e.preventDefault();
-    redFlagItems.classList.toggle('display-none');
-}
